e5cc9562a452b4ab2aad5d0553e4b62ad241d7f4
Hj-Y <1055496465@qq.com>
2019-03-30 10:54:26 +0800

    路由的创建和分配

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <router-link to="/">首页</router-link>
+    <router-link to="/login">登录页</router-link>
+    <router-view></router-view>
     
   </div>
 </template>
@@ -14,12 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
