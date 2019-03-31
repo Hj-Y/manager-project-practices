a8ff1b6ec5ffdbcf27b94708eb73ad44325cd92d
Hj-Y <1055496465@qq.com>
2019-03-31 20:55:30 +0800

    用户数据的添加&修改&删除

--- src/components/users.vue
+++ src/components/users.vue
@@ -14,13 +14,13 @@
       </el-input>
     </el-col>
     <el-col :span="2">
-      <el-button type="success" plain>添加用户</el-button>
+      <el-button type="success" plain @click="addFormVisible=true">添加用户</el-button>
     </el-col>
     <!-- </div> -->
     <!-- 表格 -->
     <el-table :data="userList" style="width: 100%" border>
       <el-table-column type="index" width="50"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="180"></el-table-column>
       <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
       <el-table-column prop="mobile" label="电话"></el-table-column>
       <el-table-column prop="mg_state" label="用户状态">
@@ -42,7 +42,13 @@
             plain
             @click="handleEdit(scope.$index,scope.row)"
           ></el-button>
-          <el-button type="danger" icon="el-icon-delete" size="mini" plain></el-button>
+          <el-button
+            type="danger"
+            icon="el-icon-delete"
+            size="mini"
+            plain
+            @click="delData(scope.row)"
+          ></el-button>
           <el-button type="warning" icon="el-icon-check" size="mini" plain></el-button>
         </template>
       </el-table-column>
@@ -54,6 +60,45 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     ></el-pagination>
+    <!-- 添加用户的对话框 -->
+    <el-dialog title="添加用户" :visible.sync="addFormVisible">
+      <el-form :model="addForm" ref="addForm" :rules="addRules">
+        <el-form-item label="用户名" label-width="100px" prop="username">
+          <el-input v-model="addForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" label-width="100px" prop="password">
+          <el-input v-model="addForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="addForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="addForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitAdd('addForm')">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 用户编辑 -->
+    <el-dialog title="编辑用户" :visible.sync="editFormVisible">
+      <el-form :model="editForm" ref="editForm" :rules="addRules">
+        <el-form-item label="用户名" label-width="100px" prop="username">
+          <el-input v-model="editForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="editForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="editForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitEdit('editForm')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -68,12 +113,46 @@ export default {
         pagenum: 1,
         pagesize: 10
       },
-      userList: []
+      userList: [],
+      addForm: {
+        username: "myaccount",
+        password: "666666",
+        email: "qq.@163.com",
+        mobile: "16666666666"
+      },
+      editForm: {
+        username: "myaccount",
+        password: "666666",
+        email: "qq.@163.com",
+        mobile: "16666666666"
+      },
+      addFormVisible: false,
+      editFormVisible: false,
+      addRules: {
+        username: [
+          { required: true, message: "用户名不能为空", trigger: "blur" },
+          { min: 3, max: 16, message: "用户名长度在3-16之间", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 6, max: 16, message: "密码长度在8-16之间", trigger: "blur" }
+        ]
+      }
     };
   },
   methods: {
-    handleEdit(index, row) {
-      console.log(index);
+    async handleEdit(index, row) {
+      // console.log(index);
+      let res = await this.$axios.get(`users/${row.id}`);
+      this.editForm = res.data.data;
+      if (res.data.meta.status === 200) {
+        this.editFormVisible = true;
+      }
+      // let res = await this.$axios.put(`users/${row.id}`);
+      // this.editForm = res.data.data;
+      // if (res.data.meta.status === 200) {
+      //   this.editFormVisible = true;
+      // }
     },
     async search() {
       let res = await this.$axios.get("users", {
@@ -88,6 +167,52 @@ export default {
     },
     stateChange(row) {
       this.$axios.put(`users/${row.id}/state/${row.mg_state}`);
+    },
+    submitAdd(formName) {
+      this.$refs[formName].validate(async valid => {
+        if (valid) {
+          let res = await this.$axios.post("users", this.addForm);
+          if (res.data.meta.status === 201) {
+            this.search();
+          }
+          //关闭弹窗
+          this.addFormVisible = false;
+        } else {
+          this.$message.warning("用户名密码都没得,你提交尼玛呢 ?");
+          return false;
+        }
+      });
+    },
+    async submitEdit(formName) {
+      let res = await this.$axios.put(`users/${this.editForm.id}`, {
+        email: this.editForm.email,
+        mobile: this.editForm.mobile
+      });
+      if (res.data.meta.status === 200) {
+        this.search();
+      }
+      //关闭弹窗
+      this.editFormVisible = false;
+    },
+    delData(row) {
+      // let res=this.$axios.delete(`users/${row.id}`)
+      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          let res = await this.$axios.delete(`users/${row.id}`);
+          if (res.data.meta.status == 200) {
+            this.search();
+          }
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
     }
   },
   created() {
