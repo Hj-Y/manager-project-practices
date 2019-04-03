e84fe00dfb4e3c2e42e06056bd01401c75efc622
Hj-Y <1055496465@qq.com>
2019-04-03 14:54:02 +0800

    路由的元信息

--- src/router.js
+++ src/router.js
@@ -14,7 +14,7 @@ import reports from "./components/reports.vue";
 
 //路由规则
 let routes = [
-  { path: "/login", component: login },
+  { path: "/login", component: login, meta: { noLogin: true } },
   {
     path: "/",
     component: index,
@@ -28,13 +28,29 @@ let routes = [
       { path: "params", component: params },
       { path: "reports", component: reports },
       //重定向
-      { path: "", redirect: "users" },
+      { path: "", redirect: "users" }
     ]
   }
 ];
 //路由创建
 const router = new VueRouter({
   routes
+});
+//注册全局前置守卫
+router.beforeEach((to, from, next) => {
+  console.log(to);
+  
+  if (to.meta.noLogin===true) {
+    //直接去
+    next();
+  } else {
+    if (window.sessionStorage.getItem("token")) {
+      next();
+    } else {
+      Vue.prototype.$message.error("守卫发现你还没有登录,去吧!皮卡丘");
+      next("/login");
+    }
+  }
 });
 //路由暴露
 export default router;
