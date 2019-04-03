baedd7be2fd38e5eb1422385fc581145ea6e8bc9
Hj-Y <1055496465@qq.com>
2019-04-03 16:33:44 +0800

    权限列表的数据渲染

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -2,7 +2,7 @@
   <div class="my-users">
     <!-- 面包屑导航 -->
     <breadcrumb sectitle="用户管理" thirdtitle="用户列表"></breadcrumb>
-    
+
     <!-- 输入框 -->
     <!-- <div> -->
     <el-col :span="2">
@@ -10,28 +10,39 @@
     </el-col>
     <!-- </div> -->
     <!-- 表格 -->
-    <el-table style="width: 100%" border>
+    <el-table style="width: 100%" border :data="rightList">
       <el-table-column type="index" width="50"></el-table-column>
-      <el-table-column  label="权限名称" width="180"></el-table-column>
-      <el-table-column  label="路径" width="180"></el-table-column>
-      <el-table-column  label="层级" width="180"></el-table-column>
-      
+      <el-table-column prop="authName" label="权限名称" width="180"></el-table-column>
+      <el-table-column prop="path" label="路径" width="180"></el-table-column>
+      <el-table-column prop="level" label="层级" width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.level==='0'">一级</span>
+          <span v-if="scope.row.level==='1'">二级</span>
+          <span v-if="scope.row.level==='2'">三级</span>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
-
 <script>
 export default {
   name: "rights",
-  created() {
-    
+  data() {
+    return {
+      rightList: []
+    };
+  },
+
+  async created() {
+    let res = await this.$axios.get("rights/list");
+    // console.log(res);
+    this.rightList = res.data.data;
   }
 };
 </script>
 
 <style lang="scss">
-.my-users{
+.my-users {
   background-color: #e9eef3;
 }
-
 </style>
