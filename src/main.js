7a51a470985e22ed1d1933ee7316d06f26d06062
Hj-Y <1055496465@qq.com>
2019-04-03 14:53:29 +0800

    install方法封装axios组件

--- src/main.js
+++ src/main.js
@@ -7,40 +7,9 @@ Vue.config.productionTip = false;
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
-//导入axios
-import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8888/api/private/v1/";
-Vue.prototype.$axios = axios;
-//设置axios拦截器
-// Add a request interceptor
-axios.interceptors.request.use(
-  function(config) {
-    //请求发送之前
-    config.headers.Authorization = window.sessionStorage.getItem("token");
-    return config;
-  },
-  function(error) {
-    // 请求失败
-    return Promise.reject(error);
-  }
-);
-
-// 响应拦截器
-axios.interceptors.response.use(
-  function(response) {
-    // 响应返回的数据
-    if (["200", "201", "204"].indexOf(response.data.meta.status) != -1) {
-      Vue.prototype.$message.success(response.data.meta.msg);
-    } else {
-      Vue.prototype.$message.warning(response.data.meta.msg);
-    }
-    return response;
-  },
-  function(error) {
-    //响应失败
-    return Promise.reject(error);
-  }
-);
+//导入axios插件
+import myaxios from './myaxios.js'
+Vue.use(myaxios)
 //导入路由
 import router from "./router.js";
 import "./assets/base.scss";
---
+++ src/myaxios.js
@@ -0,0 +1,41 @@
+
+//导入Vue
+import axios from "axios";
+
+export default {
+  install(Vue) {
+    //导入axios
+    axios.defaults.baseURL = "http://localhost:8888/api/private/v1/";
+    Vue.prototype.$axios = axios;
+    //设置axios拦截器
+    // Add a request interceptor
+    axios.interceptors.request.use(
+      function(config) {
+        //请求发送之前
+        config.headers.Authorization = window.sessionStorage.getItem("token");
+        return config;
+      },
+      function(error) {
+        // 请求失败
+        return Promise.reject(error);
+      }
+    );
+
+    // 响应拦截器
+    axios.interceptors.response.use(
+      function(response) {
+        // 响应返回的数据
+        if (["200", "201", "204"].indexOf(response.data.meta.status) != -1) {
+          Vue.prototype.$message.success(response.data.meta.msg);
+        } else {
+          Vue.prototype.$message.warning(response.data.meta.msg);
+        }
+        return response;
+      },
+      function(error) {
+        //响应失败
+        return Promise.reject(error);
+      }
+    );
+  }
+};
